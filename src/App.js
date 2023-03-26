802f810106d3eaada44615a25d916f7fc64b0067
Rishabh Kosta <rishabhkosra2255@gmail.com>
2023-03-26 18:33:29 +0530

    last-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import {Route,Routes} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home category="topstories" heading="Top Stories" />} />
+        <Route path="/sports" element={<Home category="%2F4719148" heading="Sports" />} />
+        <Route path="/science" element={<Home category="%2F-2128672765" heading="Science" />} />
+        <Route path="/environment" element={<Home category="%2F2647163" heading="Environment" />} />
+        <Route path="/technology" element={<Home category="%2F66949542" heading="Technology" />} />
+        <Route path="/entertainment" element={<Home category="%2F1081479906" heading="Entertainment" />} />
+      </Routes>
+      
     </div>
   );
 }
